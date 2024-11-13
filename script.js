df56faa74bf843858870f77d2a7a412493d8bbdf
lunledev <183235731+lunledev@users.noreply.github.com>
2024-11-12 19:32:38 -0800

    minor changes to script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+//lbs Enter weight 
+// reps - x amount of times
+// set = how many done
+const usernameInput = document.querySelector('#weight');
+const titleInput = document.querySelector('#reps');
+const contentInput = document.querySelector('#sets');
+
+weight_in = usernameInput;
+rep_in = titleInput;
+set_in = contentInput;
+
+lbs = weight_in.value;
+reps = rep_in.value;
+set = set_in.value;
+
+let array = [];
+
+
+const ObjJounalnote= {
+lbs,
+reps,
+sets, 
+
+};
+
+
+array.push(ObjJounalnote);
