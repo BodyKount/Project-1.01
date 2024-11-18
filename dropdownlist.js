3f86fa69e8efe54da72656bb093bce8582ba5785
BodyKount <cness1989@icloud.com>
2024-11-17 20:03:02 -0700

    updated html and new js file

---
+++ dropdownlist.js
@@ -0,0 +1,88 @@
+let dropDown1 = {
+    "Sunday": {
+        "Chest": ["Push-ups", "Dumbbell Press", "Bench Press","Butterfly", "Decline Bench Press", "Incline Bench Press", "Cable Seated Chest Fly", "Pec Deck Flyes", "Chest Press"],
+        "Back": ["Deadlift", "Lateral Pulldown", "Cable Seated Row", "One Arm Dumbbell Row", "Barbell Row", "Reverse Machine Fly", "Back Extension"],
+        "Arms": ["Bicep Curl", "Dumbbell Tricep Extension", "Hammer Curl", "Tricep Dip", "Tricep Kickback", "Barbell Skull Crusher", "EZ Barbell Preacher Curl", "Tricep Pushbown"],
+        "Legs": ["Squat", "Barbell Squat", "Deadlift", "Lunges", "Leg Press", "Seated Leg Curl", "Leg Extension", "Calf Raises", "Barbell Hip Thrust"],
+        "Shoulders": ["Dubbell Shoulder Press", "Seated Dumbbell Lateral Raise", "Front Raise","Reverse Fly", "Barbell Shrugs", "Kettlebell Upright Row", "Arnold Press", "Cabled Shoulder Pull"],
+        "Core": ["Plank", "Russian Twist", "Leg Raise", "Dumbbell Straight Arm Crunch", "Bicycle Crunch", "Mountain Climber", "V-Up", "Sit-up"]
+    },
+    "Monday": {
+        "Chest": ["Push-ups", "Dumbbell Press", "Bench Press","Butterfly", "Decline Bench Press", "Incline Bench Press", "Cable Seated Chest Fly", "Pec Deck Flyes", "Chest Press"],
+        "Back": ["Deadlift", "Lateral Pulldown", "Cable Seated Row", "One Arm Dumbbell Row", "Barbell Row", "Reverse Machine Fly", "Back Extension"],
+        "Arms": ["Bicep Curl", "Dumbbell Tricep Extension", "Hammer Curl", "Tricep Dip", "Tricep Kickback", "Barbell Skull Crusher", "EZ Barbell Preacher Curl", "Tricep Pushbown"],
+        "Legs": ["Squat", "Barbell Squat", "Deadlift", "Lunges", "Leg Press", "Seated Leg Curl", "Leg Extension", "Calf Raises", "Barbell Hip Thrust"],
+        "Shoulders": ["Dubbell Shoulder Press", "Seated Dumbbell Lateral Raise", "Front Raise","Reverse Fly", "Barbell Shrugs", "Kettlebell Upright Row", "Arnold Press", "Cabled Shoulder Pull"],
+        "Core": ["Plank", "Russian Twist", "Leg Raise", "Dumbbell Straight Arm Crunch", "Bicycle Crunch", "Mountain Climber", "V-Up", "Sit-up"]
+    },
+    "Tuesday": {
+        "Chest": ["Push-ups", "Dumbbell Press", "Bench Press","Butterfly", "Decline Bench Press", "Incline Bench Press", "Cable Seated Chest Fly", "Pec Deck Flyes", "Chest Press"],
+        "Back": ["Deadlift", "Lateral Pulldown", "Cable Seated Row", "One Arm Dumbbell Row", "Barbell Row", "Reverse Machine Fly", "Back Extension"],
+        "Arms": ["Bicep Curl", "Dumbbell Tricep Extension", "Hammer Curl", "Tricep Dip", "Tricep Kickback", "Barbell Skull Crusher", "EZ Barbell Preacher Curl", "Tricep Pushbown"],
+        "Legs": ["Squat", "Barbell Squat", "Deadlift", "Lunges", "Leg Press", "Seated Leg Curl", "Leg Extension", "Calf Raises", "Barbell Hip Thrust"],
+        "Shoulders": ["Dubbell Shoulder Press", "Seated Dumbbell Lateral Raise", "Front Raise","Reverse Fly", "Barbell Shrugs", "Kettlebell Upright Row", "Arnold Press", "Cabled Shoulder Pull"],
+        "Core": ["Plank", "Russian Twist", "Leg Raise", "Dumbbell Straight Arm Crunch", "Bicycle Crunch", "Mountain Climber", "V-Up", "Sit-up"]
+    },
+    "Wednesday": {
+        "Chest": ["Push-ups", "Dumbbell Press", "Bench Press","Butterfly", "Decline Bench Press", "Incline Bench Press", "Cable Seated Chest Fly", "Pec Deck Flyes", "Chest Press"],
+        "Back": ["Deadlift", "Lateral Pulldown", "Cable Seated Row", "One Arm Dumbbell Row", "Barbell Row", "Reverse Machine Fly", "Back Extension"],
+        "Arms": ["Bicep Curl", "Dumbbell Tricep Extension", "Hammer Curl", "Tricep Dip", "Tricep Kickback", "Barbell Skull Crusher", "EZ Barbell Preacher Curl", "Tricep Pushbown"],
+        "Legs": ["Squat", "Barbell Squat", "Deadlift", "Lunges", "Leg Press", "Seated Leg Curl", "Leg Extension", "Calf Raises", "Barbell Hip Thrust"],
+        "Shoulders": ["Dubbell Shoulder Press", "Seated Dumbbell Lateral Raise", "Front Raise","Reverse Fly", "Barbell Shrugs", "Kettlebell Upright Row", "Arnold Press", "Cabled Shoulder Pull"],
+        "Core": ["Plank", "Russian Twist", "Leg Raise", "Dumbbell Straight Arm Crunch", "Bicycle Crunch", "Mountain Climber", "V-Up", "Sit-up"]
+    },
+    "Thursday": {
+        "Chest": ["Push-ups", "Dumbbell Press", "Bench Press","Butterfly", "Decline Bench Press", "Incline Bench Press", "Cable Seated Chest Fly", "Pec Deck Flyes", "Chest Press"],
+        "Back": ["Deadlift", "Lateral Pulldown", "Cable Seated Row", "One Arm Dumbbell Row", "Barbell Row", "Reverse Machine Fly", "Back Extension"],
+        "Arms": ["Bicep Curl", "Dumbbell Tricep Extension", "Hammer Curl", "Tricep Dip", "Tricep Kickback", "Barbell Skull Crusher", "EZ Barbell Preacher Curl", "Tricep Pushbown"],
+        "Legs": ["Squat", "Barbell Squat", "Deadlift", "Lunges", "Leg Press", "Seated Leg Curl", "Leg Extension", "Calf Raises", "Barbell Hip Thrust"],
+        "Shoulders": ["Dubbell Shoulder Press", "Seated Dumbbell Lateral Raise", "Front Raise","Reverse Fly", "Barbell Shrugs", "Kettlebell Upright Row", "Arnold Press", "Cabled Shoulder Pull"],
+        "Core": ["Plank", "Russian Twist", "Leg Raise", "Dumbbell Straight Arm Crunch", "Bicycle Crunch", "Mountain Climber", "V-Up", "Sit-up"]
+    },
+    "Friday": {
+        "Chest": ["Push-ups", "Dumbbell Press", "Bench Press","Butterfly", "Decline Bench Press", "Incline Bench Press", "Cable Seated Chest Fly", "Pec Deck Flyes", "Chest Press"],
+        "Back": ["Deadlift", "Lateral Pulldown", "Cable Seated Row", "One Arm Dumbbell Row", "Barbell Row", "Reverse Machine Fly", "Back Extension"],
+        "Arms": ["Bicep Curl", "Dumbbell Tricep Extension", "Hammer Curl", "Tricep Dip", "Tricep Kickback", "Barbell Skull Crusher", "EZ Barbell Preacher Curl", "Tricep Pushbown"],
+        "Legs": ["Squat", "Barbell Squat", "Deadlift", "Lunges", "Leg Press", "Seated Leg Curl", "Leg Extension", "Calf Raises", "Barbell Hip Thrust"],
+        "Shoulders": ["Dubbell Shoulder Press", "Seated Dumbbell Lateral Raise", "Front Raise","Reverse Fly", "Barbell Shrugs", "Kettlebell Upright Row", "Arnold Press", "Cabled Shoulder Pull"],
+        "Core": ["Plank", "Russian Twist", "Leg Raise", "Dumbbell Straight Arm Crunch", "Bicycle Crunch", "Mountain Climber", "V-Up", "Sit-up"]
+    },
+    "Saturday": {
+        "Chest": ["Push-ups", "Dumbbell Press", "Bench Press","Butterfly", "Decline Bench Press", "Incline Bench Press", "Cable Seated Chest Fly", "Pec Deck Flyes", "Chest Press"],
+        "Back": ["Deadlift", "Lateral Pulldown", "Cable Seated Row", "One Arm Dumbbell Row", "Barbell Row", "Reverse Machine Fly", "Back Extension"],
+        "Arms": ["Bicep Curl", "Dumbbell Tricep Extension", "Hammer Curl", "Tricep Dip", "Tricep Kickback", "Barbell Skull Crusher", "EZ Barbell Preacher Curl", "Tricep Pushbown"],
+        "Legs": ["Squat", "Barbell Squat", "Deadlift", "Lunges", "Leg Press", "Seated Leg Curl", "Leg Extension", "Calf Raises", "Barbell Hip Thrust"],
+        "Shoulders": ["Dubbell Shoulder Press", "Seated Dumbbell Lateral Raise", "Front Raise","Reverse Fly", "Barbell Shrugs", "Kettlebell Upright Row", "Arnold Press", "Cabled Shoulder Pull"],
+        "Core": ["Plank", "Russian Twist", "Leg Raise", "Dumbbell Straight Arm Crunch", "Bicycle Crunch", "Mountain Climber", "V-Up", "Sit-up"]
+    }
+}
+window.onload = function() {
+    let daySelectEl = document.getElementById("day");
+    let muscleSelectEl = document.getElementById("muscle");
+    let exerciseSelectEl = document.getElementById("exercise");
+    for(let x in dropDown1) {
+        daySelectEl.options[daySelectEl.options.length] = new Option(x, x);
+    }
+
+    daySelectEl.onchange = function() {
+        //empty muscle and exercise dropdowns
+        exerciseSelectEl.length = 1;
+        muscleSelectEl.length = 1;
+        //display default value
+        for(let y in dropDown1[this.value]) {
+            muscleSelectEl.options[muscleSelectEl.options.length] = new Option(y, y);
+        }
+    }
+    
+    muscleSelectEl.onchange = function() {
+        //empty exercise dropdown
+        exerciseSelectEl.length = 1;
+        //display default value
+        let z = dropDown1[daySelectEl.value][this.value];
+        for(let i = 0; i < z.length; i++) {
+            exerciseSelectEl.options[exerciseSelectEl.options.length] = new Option(z[i], z[i]);
+        }
+    }
+}
+
+
